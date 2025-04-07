5f72b233e65aa26c930f54d64a5f35481f91cd72
oneceroonedev <oneceroonedev@gmail.com>
2025-04-07 04:50:57 +0200

    first version project m1

--- project-MF0491_3/js/footer.js
+++ project-MF0491_3/js/footer.js
@@ -1,32 +1,59 @@
+// Create Footer
 function createFooter() {
-    footer.innerHTML = `
-        <section class="footer-left">
-            <div class="logo">
-                <img src="./assets/logo/circle.svg" alt="circle-logo" />
+    return `
+        <section id="newsletter">
+            <h3>Do you have any questions?</h3>
+            <p>Let us help you!</p>
+            <div class="email-box">
+                <input name="email" type="email" placeholder="Enter your email">
+                <button class="btn-primary">Subscribe</button>
             </div>
-            <address>2972 Westheimer Rd. Santa Ana,<br>Illinois 85486</address>
         </section>
-        <section class="footer-right">
-            <ul>
-                <li><a href="#">Team</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Press</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-            </ul>
+        <section class="footer">
+            <article class="footer-left">
+                <div class="logo">
+                    <img src="/assets/logo/circle.svg" alt="circle-logo" />
+                </div>
+                <address>2972 Westheimer Rd. Santa Ana,<br>Illinois 85486</address>
+            </article>
+            <article class="footer-right">
+                <ul class="footer-links">
+                    <li><a href="/pages/404.html">Team</a></li>
+                    <li><a href="/#services">Services</a></li>
+                    <li><a href="/pages/404.html">About Us</a></li>
+                    <li><a href="/pages/404.html">Press</a></li>
+                    <li><a href="/#projects">Projects</a></li>
+                    <li><a href="/pages/404.html">Privacy Policy</a></li>
+                </ul>
+            </article>
         </section>
+        <button id="scrollTopBtn" title="Back to top"><i class="fa-solid fa-arrow-up"></i></button>
     `;
-
-    return footer;
 }
 
+// Load Footer
 function loadFooter() {
     const footerContainer = document.getElementById("footer");
     if (footerContainer) {
-        footerContainer.appendChild(createFooter());
+        footerContainer.innerHTML= createFooter();
+
+        //Scroll up button
+        const scrollBtn = document.getElementById("scrollTopBtn");
+
+        // Show/hide button on scroll
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                scrollBtn.style.display = "block";
+            } else {
+                scrollBtn.style.display = "none";
+            }
+        });
+
+        // Back to top
+        scrollBtn.addEventListener("click", () => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
     }
 }
-  
-// Asegurar que el footer se carga cuando el DOM está listo
+
 document.addEventListener("DOMContentLoaded", loadFooter);
--- project-MF0491_3/js/header.js
+++ project-MF0491_3/js/header.js
@@ -1,27 +1,45 @@
+// Create Header
 function createHeader() {
-    header.innerHTML = `
-        <div class="logo">
-            <img src="./assets/logo/circle.svg" alt="circle-logo" />
-        </div>
-        <nav>
-            <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#services">Services</a></li>
-            </ul>
-        </nav>
-        <a href="/contact" class="btn-contact">Contact Us</a>
-      `;
-  
-    return header;
-  }
-  
-  function loadHeader() {
-    const headerContainer = document.getElementById("header");
-    if (headerContainer) {
-      headerContainer.appendChild(createHeader());
+  return `
+    <div class="logo">
+      <a href="/index.html">
+        <img src="/assets/logo/circle.svg" alt="circle-logo" />
+      </a>
+    </div>
+
+    <input type="checkbox" id="nav-toggle" class="nav-toggle" />
+    <label for="nav-toggle" class="nav-toggle-icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+
+    <nav class="menu">
+      <ul>
+        <li class="active"><a href="/index.html">Home</a></li>
+        <li><a href="/#projects">Projects</a></li>
+        <li><a href="/#services">Services</a></li>
+        <li class="btn-contact-menu"><a href="/pages/contact.html" class="btn-primary">Contact Us</a></li>
+      </ul>
+    </nav>
+    <a href="/pages/contact.html" class="btn-primary btn-contact">Contact Us</a>
+  `;
+}
+
+// Load Header
+function loadHeader() {
+  const headerContainer = document.getElementById("header");
+  if (headerContainer) {
+    headerContainer.innerHTML = createHeader();
+
+    // No-scroll on mobile menu
+    const navToggle = document.getElementById("nav-toggle");
+    if (navToggle) {
+      navToggle.addEventListener("change", () => {
+        document.body.classList.toggle("no-scroll", navToggle.checked);
+      });
     }
   }
-  
-  // Asegurar que el header se carga cuando el DOM está listo
-  document.addEventListener("DOMContentLoaded", loadHeader);
+}
+
+document.addEventListener("DOMContentLoaded", loadHeader);
